f9f4599f6077f9e2fe343773c704f66c5597e53d
cam <cameronlodge93@gmail.com>
2025-01-27 18:44:39 -0800

    Added printable table

--- test_file.py
+++ test_file.py
@@ -1,17 +1,13 @@
 import sys
 
+from rich.console import Console
+from rich.table import Table
 import requests
 
 
 key = "f908849cbe14794c8cdce604c8fd547d098d2af1"
-headers = {
-    'Content-Type': 'application/json'
-}
-response = requests.get(
-    f"https://api.tiingo.com/iex/?tickers=apple&token={key}",
-    headers=headers)
-data = response.json()
-print(data)
+
+
 def get_data(stock: str):
     """
     list with a dictionary:
@@ -28,18 +24,45 @@ def get_data(stock: str):
     return data
 
 
+def pretty_print(stock):
+    """
+
+    :param stock: [ [{}], [{}] ]
+    :return:
+    """
+    table = Table("Ticker", style="magenta")
+    table.add_column("Last Price", justify="right", style="green")
+    for x in range(0, len(stock)):
+        # unpack
+        stock_info = stock[x]
+        # Get the dictionary in the list and print out a row
+        table.add_row(stock_info[0]['ticker'], str(stock_info[0]['last']))
+
+    return table
+
+
+def reset_table():
+    table = Table("Ticker", style="magenta")
+    table.add_column("Last Price", justify="right", style="green")
+
+
 def menu():
     print("Welcome!\n")
+    stock_list = []
     while True:
         stock = str(input("Look up stock or click 1 to exit: "))
 
-        if stock == 1:
+        if stock == "1":
             print("Thank you for stopping by!")
             break
         else:
-            data = get_data(stock)
-            print(data)
-            print(data[0]['last'])
+            stock = get_data(stock)
+            stock_list.append(stock)
+            table = pretty_print(stock_list)
+            console = Console(color_system="windows")
+            console.print(table)
+
+
 
 
 menu()
