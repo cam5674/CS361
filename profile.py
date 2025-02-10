27e2bdccd8a96e6fa8b0d4909df1441f21941c99
cam <cameronlodge93@gmail.com>
2025-02-10 10:50:33 -0800

    added profile class

---
+++ profile.py
@@ -0,0 +1,38 @@
+class Profile:
+
+    def __init__(self, username):
+        self._fav_stocks = []
+        self._news = []
+        self._subreddits = []
+        self._username = username
+
+
+    def get_name(self):
+        return self._username
+
+    def get_fav_stocks(self):
+        return self._fav_stocks
+
+    def get_news(self):
+        return self._news
+
+    def get_subreddits(self):
+        return self._subreddits
+
+    def set_fav_stocks(self, stock):
+
+        self._fav_stocks.append(stock)
+
+    def set_news(self, headline):
+        self._news.append(headline)
+
+    def set_subreddits(self, subreddit):
+        self._subreddits.append(subreddit)
+
+    def print_fav_stocks(self):
+        for i in self._fav_stocks:
+            print(i)
+class Storage:
+  ...
+  def __init__(self):
+      self._storage = {}
